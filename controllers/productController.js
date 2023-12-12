84695c70aa10da1693c746c95fd0406c463645e9
oreoataya <dontrichpeopledifficulut@gmail.com>
2023-12-12 21:38:50 +0800

    remove required properties and add method multipart

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,7 @@ const CustomError = require('../errors');
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
-const createProduct = async (req, res) => {
+const createProductOriginal = async (req, res) => {
   const product = await Product.create(req.body);
   res.status(StatusCodes.CREATED).json({ product });
 };
@@ -14,46 +14,53 @@ const getAllProducts = async (req, res) => {
   res.status(StatusCodes.OK).json({ products });
 };
 
-// const createProduct = async (req, res) => {
-//   // Extract product data from request body
-//   const { name, price } = req.body;
+const createProductFormMultipart = async (req, res) => {
+  // Extract product data from request body
+  const { name, price } = req.body;
 
-//   // Validate product data (you may want to add more validation)
-//   if (!name || !price) {
-//     throw new CustomError.BadRequestError('Product name and price are required.');
-//   }
+  // Validate product data (you may want to add more validation)
+  if (!name || !price) {
+    throw new CustomError.BadRequestError('Product name and price are required.');
+  }
 
-//   // Check if image file is present in the request
-//   if (!req.files || !req.files.image) {
-//     throw new CustomError.BadRequestError('Image file is required.');
-//   }
+  // Check if image file is present in the request
+  if (!req.files || !req.files.image) {
+    throw new CustomError.BadRequestError('Image file is required.');
+  }
 
-//   // Handle image upload
-//   const date = Date.now();
-//   const randomString = Math.random().toString(36).substring(2, 8);
-//   const newfileName = `${randomString}-${date}`;
+  // Handle image upload
+  const date = Date.now();
+  const randomString = Math.random().toString(36).substring(2, 8);
+  const newfileName = `${randomString}-${date}`;
 
-//   const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
-//     use_filename: true,
-//     filename: newfileName,
-//     folder: 'file-upload',
-//   });
+  const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+    use_filename: true,
+    filename: newfileName,
+    folder: 'file-upload',
+  });
 
-//   // Remove the temporary file
-//   fs.unlinkSync(req.files.image.tempFilePath);
+  // Remove the temporary file
+  fs.unlinkSync(req.files.image.tempFilePath);
 
-//   // Create a new product in the database with image URL
-//   const product = await Product.create({
-//     name,
-//     price,
-//     image: result.secure_url,
-//   });
+  // Create a new product in the database with image URL
+  const product = await Product.create({
+    name,
+    price,
+    image: result.secure_url,
+  });
 
-//   res.status(StatusCodes.CREATED).json({ product });
-
-// };
+  res.status(StatusCodes.CREATED).json({ 
+    msg: 'Product created', 
+    data: {
+      name: product.name,
+      price: product.price,
+      image: product.image,
+    }
+  });
+};
 
 module.exports = {
-  createProduct,
+  createProductOriginal,
   getAllProducts,
+  createProductFormMultipart
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct, getAllProducts } = require('../controllers/productController');
+const { createProductOriginal, getAllProducts, createProductFormMultipart} = require('../controllers/productController');
 const { uploadPostImage } = require('../controllers/uploads');
 
-router.route('/').post(createProduct).get(getAllProducts);
+router.route('/').post(createProductOriginal).get(getAllProducts);
+router.route('/multipart').post(createProductFormMultipart);
 router.route('/uploads').post(uploadPostImage);
 
 module.exports = router;
